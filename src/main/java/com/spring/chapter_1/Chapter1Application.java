df93680cd6a23974f2cc15533021c2a4ff24793c
Srijan Chandra <srijan.chandra26@gmail.com>
2024-08-16 17:25:44 +0530

    structure of spring app

---
+++ src/main/java/com/spring/chapter_1/Chapter1Application.java
@@ -0,0 +1,13 @@
+package com.spring.chapter_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+}
